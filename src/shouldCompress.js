a2c0abeede367ba1bfb1f4fda460c695c6726106
3bsa <alemubekele923@gmail.com>
2020-12-29 22:37:15 +0300

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,17 +1,18 @@
 const isAnimated = require('is-animated')
 
+
 const MIN_COMPRESS_LENGTH = process.env.MIN_COMPRESS_LENGTH || 2048
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 50 //~100KB
 const APNG_THRESH_LENGTH = MIN_COMPRESS_LENGTH * 100 //~200KB
 
 function shouldCompress(req, buffer) {
-  const { originType, originSize, webp } = req.params
+  const { originType, originSize, avif } = req.params
 
   if (!originType.startsWith('image')) return false
   if (originSize === 0) return false
   if (webp && originSize < MIN_COMPRESS_LENGTH) return false
   if (
-    !webp &&
+    !avif &&
     (originType.endsWith('png') || originType.endsWith('gif')) &&
     originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
   ) {
