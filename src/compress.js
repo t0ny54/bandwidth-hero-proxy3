a971ce444a6786c5273a853b61b05f32875646f6
Moroque <80793408+Moroque@users.noreply.github.com>
2021-07-25 00:01:45 +0200

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -102,11 +102,11 @@ function compress(req, res, input) {
     }
     function setResponseHeaders (info, imgFormat){
         res.setHeader('content-type', `image/${imgFormat}`)
-        res.setHeader('content-length', obj.info.size)
+        res.setHeader('content-length', info.size)
         let filename = (new URL(req.params.url).pathname.split('/').pop() || "image") + '.' + format
         res.setHeader('Content-Disposition', 'inline; filename="' + filename + '"' )
         res.setHeader('x-original-size', req.params.originSize)
-        res.setHeader('x-bytes-saved', req.params.originSize - obj.info.size)
+        res.setHeader('x-bytes-saved', req.params.originSize - info.size)
         res.status(200)
     }
 }
