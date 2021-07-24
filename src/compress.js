34ca26f0c08c37b050fcd8a22fec985ff389e275
Moroque <80793408+Moroque@users.noreply.github.com>
2021-07-24 21:19:15 +0200

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -31,7 +31,7 @@ function compress(req, res, input) {
         console.error(err)
         if (err) return redirect(req, res)
         //defer to gif2webp *higher latency*
-        execFile(gif2webp, '-mixed', '-m', 6, '-q', req.params.quality , '-mt', 
+        execFile(gif2webp, '-lossy','-mixed', '-m', 6, '-q', req.params.quality , '-mt', 
             `${path}.gif`,
             '-o', 
             `${path}.webp`, (convErr) => {
