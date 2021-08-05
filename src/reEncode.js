fa54ed1edb0b29e777ab4e42aa5c04e3b357f37b
Moroque <80793408+Moroque@users.noreply.github.com>
2021-08-05 13:00:12 +0200

    Update reEncode.js

--- src/reEncode.js
+++ src/reEncode.js
@@ -16,7 +16,7 @@ const VIDEO_QUALITY_MULTIPLIER = parseInt(process.env.VIDEO_QUALITY_MULTIPLIER) 
 const AUDIO_QUALITY_MULTIPLIER = parseInt(process.env.AUDIO_QUALITY_MULTIPLIER) || 2
 const MEDIA_TIMEOUT = parseInt(process.env.MEDIA_TIMEOUT) || 7200
 const VIDEO_HEIGHT_THRESH = parseInt(process.env.VIDEO_HEIGHT_THRESH) || 1080
-const VIDEO_WEBM_CPU_USED = parseInt(process.env.VIDEO_WEBM_CPU_USED) || 8
+const VIDEO_WEBM_CPU_USED = parseInt(process.env.VIDEO_WEBM_CPU_USED) || 0
 
 function reEncode(req, res, input) {
     var quality = req.params.quality
